3cb8025e11d8639c55272117ae657bcec8955c2e
geekboyisonnow <geekboyisonnow@github.com>
2021-05-13 12:14:05 -0400

    Working on Coding Challenge - Do While, set bool

--- Strings/ProgChallengeStart/Program.cs
+++ Strings/ProgChallengeStart/Program.cs
@@ -15,10 +15,30 @@ namespace ProgChallengeStart
             Console.WriteLine("Enter your guess, or -1 to give up.");
 
             // Keep track of the number of guesses and the current user guess
-
+            int count = 0;
+            int playerGuess = 0;
+            
             // Start the game and run until user quits or guesses correctly
             // HINT: You'll need a way to convert the user's input to an integer
-            
+            try {
+                Console.WriteLine("What is your guess?");
+                string guess = Console.ReadLine();
+                playerGuess = guess.int.Parse();
+                
+                if (playerGuess == theNumber) {
+                    count++;
+                    Console.WriteLine($"You are right! Only took {count} tries!");
+                } else if (playerGuess == -1) {
+                    Console.WriteLine($"Don't give up... Play again!");
+
+                } else {
+                    count++;
+                    Console.WriteLine($"Sorry try again!");
+                }
+            }
+            catch {
+                Console.WriteLine("Your guess must be an integer. Please try again!");
+            }
         }
     }
 }

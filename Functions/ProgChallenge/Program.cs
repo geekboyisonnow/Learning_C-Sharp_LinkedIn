4e3b1b8d084cf697cd42532d9f3adc2d2fb4ff62
geekboyisonnow <geekboyisonnow@github.com>
2021-05-16 09:15:39 -0400

    Program Challenge - Return for another attempt

---
+++ Functions/ProgChallenge/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ProgChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Palindrome Challenge Rules
+            // Program reads input and determines if palindrome
+            // Must ignore case and punctuation
+            // Function must be named IsPalindrome
+            // Returns two values: Boolean and integer
+            // Program runs until "exit" is typed
+
+            // Attempt ONE: Failed ;(
+            // string userStr, reverseStr = string.Empty;
+            // bool isTrue = true;  
+            // Console.Write("Enter a palindrome: ");  
+            // userStr = Console.ReadLine();  
+            // if (userStr != null)  
+            // {  
+            //     for (int i = userStr.Length - 1; i >= 0; i--)  
+            //     {  
+            //         reverseStr += userStr[i].ToString();  
+            //     }  
+            //     if (reverseStr == userStr)  
+            //     {  
+            //         Console.WriteLine("Palindrome: {0}, Length: {1}", "Correct", reverseStr.Length);  
+            //     }  
+            //     else  
+            //     {  
+            //     Console.WriteLine("String is not Palindrome: {0}, Length: {1}", "Sorry!", 0);  
+            //     }  
+            // }  
+            // Console.ReadLine();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
